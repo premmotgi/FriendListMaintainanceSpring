eb1803acc6734d04ebe94ce87e0bdaeae6c910bb
premmotgi <55761557+premmotgi@users.noreply.github.com>
2020-03-30 15:48:44 -0700

    added  servlet initialiser

--- src/main/java/com/devopstest/DevopsTestApplication.java
+++ src/main/java/com/devopstest/DevopsTestApplication.java
@@ -5,10 +5,12 @@ import com.devopstest.controller.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 //dont use component scan for spring boot, let boot itself scan packages
-public class DevopsTestApplication {
+public class DevopsTestApplication extends SpringBootServletInitializer{
 
 
 	public static void main(String[] args) {
